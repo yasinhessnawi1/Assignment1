698db83600bfcf3409602b3035442b1fb4986b83
yasinhessnawi1 <79910859+yasinhessnawi1@users.noreply.github.com>
2024-02-13 23:15:34 +0100

    back to port 8080

--- utils/port.go
+++ utils/port.go
@@ -8,14 +8,11 @@ import (
 // setUpPort sets the port for the server
 func SetUpPort() string {
 	// Get the PORT environment variable
-	err := os.Setenv("PORT", "10000")
-	if err != nil {
-		log.Println("Error setting the PORT environment variable:", err.Error())
-	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Println("$PORT has not been set. Default: 8080")
-		port = "10000"
+		port = "8080"
 	}
 	return port
 }
